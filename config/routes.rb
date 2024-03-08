085a96b286a7efeeb4a43b4a623d94edb9f545da
Mike <m.kniazevich@coaxsoft.com>
2024-03-08 14:40:41 +0200

    Add devise User

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
@@ -8,5 +7,4 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "static_pages#home"
-  get 'static_pages/home', to: 'static_pages#home'
 end
