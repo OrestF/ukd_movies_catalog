6c74764ccbbae79e5111a50a7d2e212d6df77569
Mike <m.kniazevich@coaxsoft.com>
2024-04-19 14:45:47 +0300

    Sidekiq done

--- Gemfile
+++ Gemfile
@@ -48,6 +48,8 @@ gem 'pundit'
 gem 'faraday', '~> 2.9'
 # gem 'image_processing', '~> 1.2'
 
+gem 'sidekiq'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ]
--- app/mailers/comment_mailer.rb
+++ app/mailers/comment_mailer.rb
@@ -1,7 +1,8 @@
 class CommentMailer < ApplicationMailer
   def created
     @user = params[:user]
-    @comment = params[:comment]
+    comment_to = params[:comment].commentable
+    @movie = comment_to.is_a?(Movie) ? comment_to : comment_to.commentable
     @subject = 'New comment created'
 
     mail to: @user.email, subject: @subject
